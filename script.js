2a05b8afca38a0b2763339393f1d2a90bc4b8e4a
andrzej-t <andrzej.tyrpa@gmail.com>
2021-08-31 01:07:32 +0200

    Changing endpoints

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://shrouded-plateau-38333.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://shrouded-plateau-38333.herokuapp.com/v1/trello/';
-   // const trelloApiRoot = 'https://api.trello.com/1';
+   const apiRoot = 'http://localhost:8080/v1/';
+   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
@@ -13,7 +12,7 @@ $(document).ready(function() {
    getAllTasks();
 
    function getAllAvailableBoards(callback, callbackArgs) {
-      var requestUrl = trelloApiRoot + 'getTrelloBoards';
+      var requestUrl = trelloApiRoot + 'boards';
 
       $.ajax({
          url: requestUrl,
@@ -64,7 +63,7 @@ $(document).ready(function() {
    }
 
    function getAllTasks() {
-      const requestUrl = apiRoot + 'getTasks';
+      const requestUrl = apiRoot + 'tasks';
 
       $.ajax({
          url: requestUrl,
@@ -85,7 +84,7 @@ $(document).ready(function() {
       var taskId = parentEl.attr('data-task-id');
       var taskTitle = parentEl.find('[data-task-name-input]').val();
       var taskContent = parentEl.find('[data-task-content-input]').val();
-      var requestUrl = apiRoot + 'updateTask';
+      var requestUrl = apiRoot + 'tasks';
 
       $.ajax({
          url: requestUrl,
@@ -109,17 +108,16 @@ $(document).ready(function() {
    function handleTaskDeleteRequest() {
       var parentEl = $(this).parents('[data-task-id]');
       var taskId = parentEl.attr('data-task-id');
-      var requestUrl = apiRoot + 'deleteTask';
+      var requestUrl = apiRoot + 'tasks';
 
-      $.ajax({
-         url: requestUrl + '/?' + $.param({
-            taskId: taskId
-         }),
+        $.ajax({
+         url: requestUrl + '/' + taskId,
          method: 'DELETE',
          success: function() {
             parentEl.slideUp(400, function() { parentEl.remove(); });
          }
       })
+
    }
 
    function handleTaskSubmitRequest(event) {
@@ -128,7 +126,7 @@ $(document).ready(function() {
       var taskTitle = $(this).find('[name="title"]').val();
       var taskContent = $(this).find('[name="content"]').val();
 
-      var requestUrl = apiRoot + 'createTask';
+      var requestUrl = apiRoot + 'tasks';
 
       $.ajax({
          url: requestUrl,
@@ -140,6 +138,7 @@ $(document).ready(function() {
             title: taskTitle,
             content: taskContent
          }),
+
         complete: function(data) {
            if (data.status === 200) {
               getAllTasks();
@@ -169,7 +168,7 @@ $(document).ready(function() {
    }
 
    function handleCardCreationRequest(event) {
-      var requestUrl = trelloApiRoot + 'createTrelloCard';
+      var requestUrl = trelloApiRoot + 'cards';
       var $relatedTaskRow = $(event.target).parents('[data-task-id]');
       var relatedTaskId = $relatedTaskRow.attr('data-task-id');
       var relatedTask = availableTasks[relatedTaskId];
@@ -207,136 +206,3 @@ $(document).ready(function() {
    $tasksContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
    $tasksContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
 });
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-
-//   var apiRoot = 'https://shrouded-plateau-38333.herokuapp.com/v1/task/';
-//   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-//   var tasksContainer = $('[data-tasks-container]');
-
-//   // init
-//   getAllTasks();
-
-//   function createElement(data) {
-//     var element = $(datatableRowTemplate).clone();
-
-//     element.attr('data-task-id', data.id);
-//     element.find('[data-task-name-section] [data-task-name-paragraph]').text(data.title);
-//     element.find('[data-task-name-section] [data-task-name-input]').val(data.title);
-
-//     element.find('[data-task-content-section] [data-task-content-paragraph]').text(data.content);
-//     element.find('[data-task-content-section] [data-task-content-input]').val(data.content);
-
-//     return element;
-//   }
-
-//   function handleDatatableRender(data) {
-//     tasksContainer.empty();
-//     data.forEach(function(task) {
-//       createElement(task).appendTo(tasksContainer);
-//     });
-//   }
-
-//   function getAllTasks() {
-//     var requestUrl = apiRoot + 'getTasks';
-
-//     $.ajax({
-//       url: requestUrl,
-//       method: 'GET',
-//         success: handleDatatableRender
-//      });
-//   }
-
-//   function handleTaskUpdateRequest() {
-//     var parentEl = $(this).parent().parent();
-//     var taskId = parentEl.attr('data-task-id');
-//     var taskTitle = parentEl.find('[data-task-name-input]').val();
-//     var taskContent = parentEl.find('[data-task-content-input]').val();
-//     var requestUrl = apiRoot + 'updateTask';
-
-//     $.ajax({
-//       url: requestUrl,
-//       method: "PUT",
-//       processData: false,
-//       contentType: "application/json; charset=utf-8",
-//       dataType: 'json',
-//       data: JSON.stringify({
-//         id: taskId,
-//         title: taskTitle,
-//         content: taskContent
-//       }),
-//       success: function(data) {
-//         parentEl.attr('data-task-id', data.id).toggleClass('datatable__row--editing');
-//         parentEl.find('[data-task-name-paragraph]').text(taskTitle);
-//         parentEl.find('[data-task-content-paragraph]').text(taskContent);
-//       }
-//     });
-//   }
-
-//   function handleTaskDeleteRequest() {
-//       var parentEl = $(this).parent().parent();
-//       var taskId = parentEl.attr('data-task-id');
-//       var requestUrl = apiRoot + 'deleteTask';
-
-//       $.ajax({
-//         url: requestUrl + '/?' + $.param({
-//           taskId: taskId
-//         }),
-//         method: 'DELETE',
-//         success: function() {
-//           parentEl.slideUp(400, function() { parentEl.remove(); });
-//         }
-//       })
-//     }
-
-//   function handleTaskSubmitRequest(event) {
-//     event.preventDefault();
-
-//     var taskTitle = $(this).find('[name="title"]').val();
-//     var taskContent = $(this).find('[name="content"]').val();
-
-//     var requestUrl = apiRoot + 'createTask';
-
-//     $.ajax({
-//       url: requestUrl,
-//       method: 'POST',
-//       processData: false,
-//       contentType: "application/json; charset=utf-8",
-//       dataType: 'json',
-//       data: JSON.stringify({
-//         title: taskTitle,
-//         content: taskContent
-//       }),
-//       complete: function(data) {
-//         if(data.status === 200) {
-//           getAllTasks();
-//         }
-//      }
-//     });
-//   }
-
-//   function toggleEditingState() {
-//     var parentEl = $(this).parent().parent();
-//     parentEl.toggleClass('datatable__row--editing');
-
-//     var taskTitle = parentEl.find('[data-task-name-paragraph]').text();
-//     var taskContent = parentEl.find('[data-task-content-paragraph]').text();
-
-//     parentEl.find('[data-task-name-input]').val(taskTitle);
-//     parentEl.find('[data-task-content-input]').val(taskContent);
-//   }
-
-//   $('[data-task-add-form]').on('submit', handleTaskSubmitRequest);
-
-//   tasksContainer.on('click','[data-task-edit-button]', toggleEditingState);
-//   tasksContainer.on('click','[data-task-edit-abort-button]', toggleEditingState);
-//   tasksContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
-//   tasksContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
-// });
